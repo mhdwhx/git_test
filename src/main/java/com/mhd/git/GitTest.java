8b7523b452b9d0bb1552fbc287c6a796bbadebf2
mhd <mhd@atguigu.com>
2023-10-28 13:34:40 +0800

    push commit

--- src/main/java/com/mhd/git/GitTest.java
+++ src/main/java/com/mhd/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello,git4");
         System.out.println("master test");
         System.out.println("hot_fix_test");
+        System.out.println("push_test");
     }
 }
