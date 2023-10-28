cff5ca2c0dfe48f1a55baa9e988301746f9e642f
mhd <mhd@atguigu.com>
2023-10-28 12:19:04 +0800

    first commit

---
+++ src/main/java/com/mhd/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.mhd.git;
+
+/**
+ * @author Administrator
+ * @create 2023-10-28 12:13
+ * @Description
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
